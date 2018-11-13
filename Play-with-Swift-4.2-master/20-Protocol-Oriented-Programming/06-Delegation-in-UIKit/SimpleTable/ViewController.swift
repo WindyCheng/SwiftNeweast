753458c51d374f5b98f85424d134a0eba56fa373
WindyCheng <liusheng.cheng@17weima.com>
2018-11-13 16:03:48 +0800

    初始化。

---
+++ Play-with-Swift-4.2-master/20-Protocol-Oriented-Programming/06-Delegation-in-UIKit/SimpleTable/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by liuyubobo on 6/23/16.
+//  Copyright © 2016 Yubo Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var tableView: UITableView = UITableView()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView = UITableView(frame: view.frame)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello"
+        
+        return cell
+    }
+
+}
+
