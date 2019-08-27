7a87b4fb15b69f22eb419493e98103a090dd4721
WindyCheng <liusheng.cheng@17weima.com>
2019-08-27 09:21:38 +0800

    Swift 5.1更新。

---
+++ Play-with-Swift-5.1-master/10-Closure/03-1-Trailing-Closure.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var arr:[Int] = []
+for _ in 0..<10{
+    arr.append(Int(arc4random() % 1000))   //swift 3
+}
+
+/*
+ 由于以下代码在swift3中的改动都差不多: arr.sort()改成arr.sorted(by: )，在这里我直接将下面的代码替换成正确的代码
+ swift 3中sort()的API改成了sorted(by: ) 这点和python比较像
+ 在这里引用一下慕课网用户"慕大神仙"的解释:
+ 如果大家使用的是swift3.0版本，那么需要注意：
+ 而在3.0版本中以上方法被修改为 sorted() sort()
+ 新版中 sorted()是不会改变原数组的值，而sort()是会改变原数组的值，这点与2.0是不一样的
+ */
+
+arr.sorted(by: { a , b in return a > b})
+
+
+// Trailing Closure
+arr.sort(){ a , b in
+    return a > b
+}
+
+arr.sort{ a , b in
+    return a > b
+}
+
+
+arr.map{ number -> String in
+    
+    var number = number
+    
+    var res = ""
+    repeat{
+        res = String(number % 2) + res
+        number /= 2
+    }while number != 0
+    return res
+}
+
+
