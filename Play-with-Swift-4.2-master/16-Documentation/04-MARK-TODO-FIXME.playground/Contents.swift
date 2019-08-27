56288b259acb4050ab82ed3df41ced1edf90a6f7
WindyCheng <liusheng.cheng@17weima.com>
2019-08-27 09:08:13 +0800

    更新5.1

--- Play-with-Swift-4.2-master/16-Documentation/04-MARK-TODO-FIXME.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-let myBike = Bicycle(style: .Road, gearing: .Fixed, handlebar: .Riser, frameSize: 38)
-
-myBike.travel(distance: 100)
--- Play-with-Swift-4.2-master/20-Protocol-Oriented-Programming/05-Swift-is-Protocol-Oriented.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var a: Int = 3
-
-var arr: Array<Int> = [1,2,3]
